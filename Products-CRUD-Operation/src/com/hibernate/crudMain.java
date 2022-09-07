04fa39f95fd53d7ffd60b7e2fd87b492dd6a9b66
Subham kr Gupta <subham7031979@gmail.com>
2022-09-07 16:10:05 +0530

    Completed : Create operation

---
+++ Products-CRUD-Operation/src/com/hibernate/crudMain.java
@@ -0,0 +1,16 @@
+package com.hibernate;
+
+public class crudMain {
+
+	public static void main(String[] args) throws Exception {
+		
+		Create input = new Create();
+		
+		
+		input.inputUser();
+		input.createData();
+		
+		
+	}
+
+}
